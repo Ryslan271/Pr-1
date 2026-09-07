70ad26891f9723d3ff0dca153b15090c1ee05f69
Ruslan <Ruslan@DESKTOP-PSCEKO1>
2026-09-07 16:05:13 +0300

    удаление класса SetWrapper

--- Program.cs
+++ Program.cs
@@ -12,17 +12,17 @@ namespace Pr_1_1
             var alf = new HashSet<char>("абвгдеёжзийклмнопрстуфхцчшщъыьэюя"); // алфовит
 
             // Первое задание "Даны множества А и В. Найти объединение, пересечение и разность множеств"
-            var A1 = new SetWrapper<int> ([2, 4, 6, 8, 10]);
-            var B1 = new SetWrapper<int>([4, 8, 12, 16, 20]);
+            var A1 = new Set<int> ([2, 4, 6, 8, 10]);
+            var B1 = new Set<int>([4, 8, 12, 16, 20]);
 
-            var A2 = new SetWrapper<int> ([3, 6, 9, 12, 15 ]);
-            var B2 = new SetWrapper<int> ([1, 3, 5, 7, 9]);
+            var A2 = new Set<int> ([3, 6, 9, 12, 15 ]);
+            var B2 = new Set<int> ([1, 3, 5, 7, 9]);
 
-            var A3 = new SetWrapper<double> ([1, 0.5, 0.33, 0.25]);
-            var B3 = new SetWrapper<double> ([1, 0.5, 0.25, 0.125, 0.0625]);
+            var A3 = new Set<double> ([1, 0.5, 0.33, 0.25]);
+            var B3 = new Set<double> ([1, 0.5, 0.25, 0.125, 0.0625]);
 
-            var A4 = new SetWrapper<double> ([0.1, 0.2, 0.3]);
-            var B4 = new SetWrapper<double> ([0, 0.1, 0.05, 0.033]);
+            var A4 = new Set<double> ([0.1, 0.2, 0.3]);
+            var B4 = new Set<double> ([0, 0.1, 0.05, 0.033]);
 
             //Вывод результата первого задания
             // GetResultOneTask(A1, B1);
@@ -31,20 +31,20 @@ namespace Pr_1_1
             // GetResultOneTask(A4, B4);
 
             //Второе задание "Найти A∩(B∪C), (A∪B)∩C и U\(A∪B∪C)"
-            var A5 = new SetWrapper<int>([1, 2, 4, 5, 6]);
-            var B5 = new SetWrapper<int>([1, 4, 5, 7]);
-            var C5 = new SetWrapper<int>([2, 6, 7, 8]);
-            var U5 = new SetWrapper<int>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+            var A5 = new Set<int>([1, 2, 4, 5, 6]);
+            var B5 = new Set<int>([1, 4, 5, 7]);
+            var C5 = new Set<int>([2, 6, 7, 8]);
+            var U5 = new Set<int>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
-            var A6 = new SetWrapper<char>(['a', 'b', 'c']);
-            var B6 = new SetWrapper<char>(['c', 'd', 'e']);
-            var C6 = new SetWrapper<char>(['a', 'e', 'f']);
-            var U6 = new SetWrapper<char>(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']);
+            var A6 = new Set<char>(['a', 'b', 'c']);
+            var B6 = new Set<char>(['c', 'd', 'e']);
+            var C6 = new Set<char>(['a', 'e', 'f']);
+            var U6 = new Set<char>(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']);
 
-            var A7 = new SetWrapper<char>(['л', 'о', 'г', 'и', 'к', 'а']);
-            var B7 = new SetWrapper<char>(['у', 'р', 'о', 'к']);
-            var C7 = new SetWrapper<char>(['г', 'р', 'у', 'п', 'п', 'а']);
-            var U7 = new SetWrapper<char>();
+            var A7 = new Set<char>(['л', 'о', 'г', 'и', 'к', 'а']);
+            var B7 = new Set<char>(['у', 'р', 'о', 'к']);
+            var C7 = new Set<char>(['г', 'р', 'у', 'п', 'п', 'а']);
+            var U7 = new Set<char>();
             foreach (var item in alf)
                 U7.Add(item);
 
@@ -57,23 +57,12 @@ namespace Pr_1_1
         }
 
         /// <summary>
-        /// Вывод заданных данных
-        /// </summary>
-        /// <param name="items">массив множества</param>
-        protected static void GetInitialData<T>(Dictionary<string, SetWrapper<T>> items) 
-        {
-            foreach (var item in items)
-                Console.WriteLine($"{item.Key} = [{string.Join(", ", item.Value.AllItems())}]");
-            Console.WriteLine();
-        }
-
-        /// <summary>
         /// Вывод и подсчет данных по первому заданию
         /// </summary>
-        protected static void GetResultOneTask<T>(SetWrapper<T> A, SetWrapper<T> B)
+        protected static void GetResultOneTask<T>(Set<T> A, Set<T> B)
         {
             GetInitialData(
-                new Dictionary<string, SetWrapper<T>>()
+                new Dictionary<string, Set<T>>()
                     {
                         ["A"] = A,
                         ["B"] = B
@@ -88,10 +77,10 @@ namespace Pr_1_1
         /// <summary>
         /// Вывод и подсчет данных по второму заданию заданию
         /// </summary>
-        protected static void GetResultTwoTask<T>(SetWrapper<T> A, SetWrapper<T> B, SetWrapper<T> C, SetWrapper<T> U)
+        protected static void GetResultTwoTask<T>(Set<T> A, Set<T> B, Set<T> C, Set<T> U)
         {
             GetInitialData(
-                new Dictionary<string, SetWrapper<T>>()
+                new Dictionary<string, Set<T>>()
                 {
                     ["A"] = A,
                     ["B"] = B,
@@ -104,21 +93,59 @@ namespace Pr_1_1
             Console.WriteLine($"U \\ (A + B + C) = [{string.Join(", ", U.Difference(A.Union(B.Union(C))).AllItems())}]"); // U \ (A ∪ B ∪ C)
             Console.WriteLine();
         }
+
+        /// <summary>
+        /// Вывод первоначальных данных
+        /// </summary>
+        /// <param name="items">массив множества</param>
+        protected static void GetInitialData<T>(Dictionary<string, Set<T>> items)
+        {
+            foreach (var item in items)
+                Console.WriteLine($"{item.Key} = [{string.Join(", ", item.Value.AllItems())}]");
+            Console.WriteLine();
+        }
+
     }
 
-    public abstract class Sets<T>
+    public class Set<T> : IEnumerable<T>
     {
-        public abstract void Add(T item);
-        public abstract bool Contains(T item);
-        public abstract void Remove(T item);
+        private readonly HashSet<T> _sets;
 
-        public abstract IEnumerable<T> AllItems();
-        protected abstract Sets<T> CreateEmptySet();
+        public void Add(T item) => _sets.Add(item);
+
+        public bool Contains(T item) => _sets.Contains(item);
+
+        public void Remove(T item) => _sets.Remove(item);
+
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="items"><T></param>
+        public Set() =>
+            _sets = [];
+
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="items">List<T> items</param>
+        public Set(List<T> items) =>
+            _sets = [.. items];
+
+        // Служебные методы абстракных классов
+        protected Set<T> CreateEmptySet() =>
+            new();
+
+        /// <summary>
+        /// Вывод всех элементов класса
+        /// </summary>
+        /// <returns>IEnumerable<T></returns>
+        public IEnumerable<T> AllItems() =>
+            _sets;
 
         /// <summary>
         /// Обьединение множеств
         /// </summary>
-        public Sets<T> Union(Sets<T> other)
+        public Set<T> Union(Set<T> other)
         {
             var result = CreateEmptySet();
             foreach (var item in this.AllItems())
@@ -128,10 +155,10 @@ namespace Pr_1_1
             return result;
         }
 
-        /// <summary>
+        /// <summary> 
         /// Пересечения множеств
         /// </summary>
-        public Sets<T> Intersect(Sets<T> other)
+        public Set<T> Intersect(Set<T> other)
         {
             var result = CreateEmptySet();
             foreach (var item in this.AllItems())
@@ -144,7 +171,7 @@ namespace Pr_1_1
         /// <summary>
         /// Разность множеств
         /// </summary>
-        public Sets<T> Difference(Sets<T> other)
+        public Set<T> Difference(Set<T> other)
         {
             var result = CreateEmptySet();
             foreach (var item in this.AllItems())
@@ -153,43 +180,6 @@ namespace Pr_1_1
 
             return result;
         }
-    }
-
-    public class SetWrapper<T> : Sets<T>, IEnumerable<T>
-    {
-        private readonly HashSet<T> _sets;
-
-        /// <summary>
-        /// Конструктор класса
-        /// </summary>
-        /// <param name="items"><T></param>
-        public SetWrapper() =>
-            _sets = [];
-
-        /// <summary>
-        /// Конструктор класса
-        /// </summary>
-        /// <param name="items">List<T> items</param>
-        public SetWrapper(List<T> items) =>
-            _sets = [.. items];
-
-        // Инициализация обязательных методов
-        public override void Add(T item) => _sets.Add(item);
-
-        public override bool Contains(T item) => _sets.Contains(item);
-
-        public override void Remove(T item) => _sets.Remove(item);
-
-        // Служебные методы абстракных классов
-        protected override Sets<T> CreateEmptySet() =>
-            new SetWrapper<T>();
-
-        /// <summary>
-        /// Вывод всех элементов класса
-        /// </summary>
-        /// <returns>IEnumerable<T></returns>
-        public override IEnumerable<T> AllItems() =>
-            _sets;
 
         //Инициализация методов IEnumerable
         IEnumerator IEnumerable.GetEnumerator() =>

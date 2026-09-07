cbc15a2dcbdc985a2e50660f26240a848adf9b59
Ruslan <Ruslan@DESKTOP-PSCEKO1>
2026-09-07 16:51:34 +0300

    Перевод Set в отдельный файл, переопределение операторов, переопределение ToString(), переделал вывод

--- Program.cs
+++ Program.cs
@@ -1,34 +1,29 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace Pr_1_1
+﻿namespace Pr_1_1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var alf = new HashSet<char>("абвгдеёжзийклмнопрстуфхцчшщъыьэюя"); // алфовит
+            var alf = new HashSet<char>("абвгдеёжзийклмнопрстуфхцчшщъыьэюя"); // алфавит
 
             // Первое задание "Даны множества А и В. Найти объединение, пересечение и разность множеств"
-            var A1 = new Set<int> ([2, 4, 6, 8, 10]);
+            var A1 = new Set<int>([2, 4, 6, 8, 10]);
             var B1 = new Set<int>([4, 8, 12, 16, 20]);
 
-            var A2 = new Set<int> ([3, 6, 9, 12, 15 ]);
-            var B2 = new Set<int> ([1, 3, 5, 7, 9]);
+            var A2 = new Set<int>([3, 6, 9, 12, 15]);
+            var B2 = new Set<int>([1, 3, 5, 7, 9]);
 
-            var A3 = new Set<double> ([1, 0.5, 0.33, 0.25]);
-            var B3 = new Set<double> ([1, 0.5, 0.25, 0.125, 0.0625]);
+            var A3 = new Set<double>([1, 0.5, 0.33, 0.25]);
+            var B3 = new Set<double>([1, 0.5, 0.25, 0.125, 0.0625]);
 
-            var A4 = new Set<double> ([0.1, 0.2, 0.3]);
-            var B4 = new Set<double> ([0, 0.1, 0.05, 0.033]);
+            var A4 = new Set<double>([0.1, 0.2, 0.3]);
+            var B4 = new Set<double>([0, 0.1, 0.05, 0.033]);
 
-            //Вывод результата первого задания
-            // GetResultOneTask(A1, B1);
-            // GetResultOneTask(A2, B2);
-            // GetResultOneTask(A3, B3);
-            // GetResultOneTask(A4, B4);
+            // Вывод результата первого задания
+            //GetResultOneTask(A1, B1);
+            //GetResultOneTask(A2, B2);
+            //GetResultOneTask(A3, B3);
+            //GetResultOneTask(A4, B4);
 
             //Второе задание "Найти A∩(B∪C), (A∪B)∩C и U\(A∪B∪C)"
             var A5 = new Set<int>([1, 2, 4, 5, 6]);
@@ -53,7 +48,6 @@ namespace Pr_1_1
             GetResultTwoTask(A6, B6, C6, U6);
             GetResultTwoTask(A7, B7, C7, U7);
 
-
         }
 
         /// <summary>
@@ -63,19 +57,19 @@ namespace Pr_1_1
         {
             GetInitialData(
                 new Dictionary<string, Set<T>>()
-                    {
-                        ["A"] = A,
-                        ["B"] = B
-                    });
+                {
+                    ["A"] = A,
+                    ["B"] = B
+                });
 
-            Console.WriteLine($"A + B = [{string.Join(", ", A.Union(B).AllItems())}]");
-            Console.WriteLine($"A - B = [{string.Join(", ", A.Intersect(B).AllItems())}]");
-            Console.WriteLine($"A \\ B = [{string.Join(", ", A.Difference(B).AllItems())}]");
+            Console.WriteLine($"A + B = {A + B}");
+            Console.WriteLine($"A - B = {A - B}");
+            Console.WriteLine($"A \\ B = {A / B}");
             Console.WriteLine();
         }
 
         /// <summary>
-        /// Вывод и подсчет данных по второму заданию заданию
+        /// Вывод и подсчет данных по второму заданию
         /// </summary>
         protected static void GetResultTwoTask<T>(Set<T> A, Set<T> B, Set<T> C, Set<T> U)
         {
@@ -88,9 +82,9 @@ namespace Pr_1_1
                     ["U"] = U
                 });
 
-            Console.WriteLine($"A - (B + C) = [{string.Join(", ", A.Intersect(B.Union(C)).AllItems())}]"); // A ∩ (B ∪ C)
-            Console.WriteLine($"(A + B) - C = [{string.Join(", ", (A.Union(B)).Intersect(C).AllItems())}]"); // (A ∪ B) ∩ C
-            Console.WriteLine($"U \\ (A + B + C) = [{string.Join(", ", U.Difference(A.Union(B.Union(C))).AllItems())}]"); // U \ (A ∪ B ∪ C)
+            Console.WriteLine($"A - (B + C) = {A - (B + C)}"); // A ∩ (B ∪ C)
+            Console.WriteLine($"(A + B) - C = {(A + B) - C}"); // (A ∪ B) ∩ C
+            Console.WriteLine($"U \\ (A + B + C) = {U / (A + B + C)}"); // U \ (A ∪ B ∪ C)
             Console.WriteLine();
         }
 
@@ -101,91 +95,8 @@ namespace Pr_1_1
         protected static void GetInitialData<T>(Dictionary<string, Set<T>> items)
         {
             foreach (var item in items)
-                Console.WriteLine($"{item.Key} = [{string.Join(", ", item.Value.AllItems())}]");
+                Console.WriteLine($"{item.Key} = [{string.Join(", ", item.Value.AllItems)}]");
             Console.WriteLine();
         }
-
-    }
-
-    public class Set<T> : IEnumerable<T>
-    {
-        private readonly HashSet<T> _sets;
-
-        public void Add(T item) => _sets.Add(item);
-
-        public bool Contains(T item) => _sets.Contains(item);
-
-        public void Remove(T item) => _sets.Remove(item);
-
-        /// <summary>
-        /// Конструктор класса
-        /// </summary>
-        /// <param name="items"><T></param>
-        public Set() =>
-            _sets = [];
-
-        /// <summary>
-        /// Конструктор класса
-        /// </summary>
-        /// <param name="items">List<T> items</param>
-        public Set(List<T> items) =>
-            _sets = [.. items];
-
-        // Служебные методы абстракных классов
-        protected Set<T> CreateEmptySet() =>
-            new();
-
-        /// <summary>
-        /// Вывод всех элементов класса
-        /// </summary>
-        /// <returns>IEnumerable<T></returns>
-        public IEnumerable<T> AllItems() =>
-            _sets;
-
-        /// <summary>
-        /// Обьединение множеств
-        /// </summary>
-        public Set<T> Union(Set<T> other)
-        {
-            var result = CreateEmptySet();
-            foreach (var item in this.AllItems())
-                result.Add(item);
-            foreach (var item in other.AllItems())
-                result.Add(item);
-            return result;
-        }
-
-        /// <summary> 
-        /// Пересечения множеств
-        /// </summary>
-        public Set<T> Intersect(Set<T> other)
-        {
-            var result = CreateEmptySet();
-            foreach (var item in this.AllItems())
-                if (other.Contains(item))
-                    result.Add(item);
-
-            return result;
-        }
-
-        /// <summary>
-        /// Разность множеств
-        /// </summary>
-        public Set<T> Difference(Set<T> other)
-        {
-            var result = CreateEmptySet();
-            foreach (var item in this.AllItems())
-                if (!other.Contains(item))
-                    result.Add(item);
-
-            return result;
-        }
-
-        //Инициализация методов IEnumerable
-        IEnumerator IEnumerable.GetEnumerator() =>
-            (IEnumerator)AllItems();
-
-        public IEnumerator<T> GetEnumerator() =>
-            throw new NotImplementedException();
     }
 }
---
+++ Set.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+
+namespace Pr_1_1
+{
+    public class Set<T> : IEnumerable<T>
+    {
+        private readonly HashSet<T> _sets;
+
+        public IEnumerable<T> AllItems => _sets;
+
+        public void Add(T item) => _sets.Add(item);
+
+        public bool Contains(T item) => _sets.Contains(item);
+
+        public void Remove(T item) => _sets.Remove(item);
+
+
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="items"><T></param>
+        public Set() =>
+            _sets = [];
+
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        /// <param name="items">List<T> items</param>
+        public Set(List<T> items) =>
+            _sets = [.. items];
+
+        // Создание нового обьекта класса
+        protected Set<T> CreateEmptySet() =>
+            new();
+
+        /// <summary>
+        /// Обьединение множеств
+        /// </summary>
+        public static Set<T> operator +(Set<T> a, Set<T> b)
+        {
+            var result = a.CreateEmptySet();
+            foreach (var item in a.AllItems)
+                result.Add(item);
+            foreach (var item in b.AllItems)
+                result.Add(item);
+            return result;
+        }
+
+        /// <summary> 
+        /// Пересечения множеств
+        /// </summary>
+        public static Set<T> operator -(Set<T> a, Set<T> b)
+        {
+            var result = a.CreateEmptySet();
+            foreach (var item in a.AllItems)
+                if (b.Contains(item))
+                    result.Add(item);
+            return result;
+        }
+
+        /// <summary>
+        /// Разность множеств
+        /// </summary>
+        public static Set<T> operator /(Set<T> a, Set<T> b)
+        {
+            var result = a.CreateEmptySet();
+            foreach (var item in a.AllItems)
+                if (!b.Contains(item))
+                    result.Add(item);
+            return result;
+        }
+
+        public override string ToString() =>
+            $"[{string.Join(", ", AllItems)}]";
+
+        //Инициализация методов IEnumerable
+        IEnumerator IEnumerable.GetEnumerator() =>
+            (IEnumerator)AllItems;
+
+        public IEnumerator<T> GetEnumerator() =>
+            _sets.GetEnumerator();
+    }
+}
